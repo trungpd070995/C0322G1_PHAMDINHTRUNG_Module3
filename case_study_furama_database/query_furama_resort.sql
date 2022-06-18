3b4c323228852812acc9a2d9990be252f6c6aff1
unknown <you@example.com>
2022-06-18 21:38:57 +0700

    up

--- case_study_furama_database/query_furama_resort.sql
+++ case_study_furama_database/query_furama_resort.sql
@@ -30,3 +30,35 @@ left join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
 left join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
 left join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
 group by ma_khach_hang;
+
+-- TASK - 6 : Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ 
+-- chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+select dich_vu.ma_dich_vu, dich_vu.ten_dich_vu, dich_vu.dien_tich, dich_vu.chi_phi_thue, loai_dich_vu.ten_loai_dich_vu
+from dich_vu
+join loai_dich_vu on loai_dich_vu.ma_loai_dich_vu = dich_vu.ma_loai_dich_vu
+join hop_dong on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+where hop_dong.ma_dich_vu not in( 
+select hop_dong.ma_dich_vu
+where year(hop_dong.ngay_lam_hop_dong) = 2021 and (month(hop_dong.ngay_lam_hop_dong)between 1 and 3)
+group by hop_dong.ma_dich_vu);
+
+-- TASK - 7 : Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ 
+-- đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+select dich_vu.ma_dich_vu, dich_vu.ten_dich_vu, dich_vu.dien_tich, dich_vu.so_nguoi_toi_da, dich_vu.chi_phi_thue, loai_dich_vu.ten_loai_dich_vu
+from dich_vu
+join loai_dich_vu on loai_dich_vu.ma_loai_dich_vu = dich_vu.ma_loai_dich_vu
+join hop_dong on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+where year(hop_dong.ngay_lam_hop_dong) = 2020 and year(hop_dong.ngay_lam_hop_dong) not in( 
+select year(hop_dong.ngay_lam_hop_dong)
+where year(hop_dong.ngay_lam_hop_dong) = 2021);
+
+-- TASK - 8 : Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+select khach_hang.ho_va_ten
+from khach_hang
+group by khach_hang.ho_va_ten
+union 
+select khach_hang.ho_va_ten
+from khach_hang
+group by khach_hang.ho_va_ten;
+
+
