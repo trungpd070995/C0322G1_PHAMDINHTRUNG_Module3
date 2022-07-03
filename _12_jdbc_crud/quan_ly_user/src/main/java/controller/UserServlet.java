0c37190acb420abbd49ada04eb45a44266711266
unknown <you@example.com>
2022-07-03 15:04:59 +0700

    up

---
+++ _12_jdbc_crud/quan_ly_user/src/main/java/controller/UserServlet.java
@@ -0,0 +1,111 @@
+package controller;
+
+import model.User;
+import service.IUserService;
+import service.UserService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "UserServlet", urlPatterns = "/users")
+public class UserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    IUserService userService = new UserService();
+    static List<User> listUser = new ArrayList<>();
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "create":
+                insertUser(request, response);
+                break;
+            case "edit":
+                updateUser(request, response);
+                break;
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+
+        switch (action) {
+            case "create":
+                showNewForm(request, response);
+                break;
+            case "edit":
+                showEditForm(request, response);
+                break;
+            case "delete":
+                try {
+                    deleteUser(request, response);
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+                break;
+            case "search" :
+                searchUser(request, response);
+                break;
+            default:
+                listUser(request, response);
+                break;
+        }
+    }
+
+    private void listUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<User> listUser = userService.selectAllUsers();
+        request.setAttribute("listUser", listUser);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user/list.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void searchUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<User> listUser = userService.selectAllUsers();
+        String countrySearch = request.getParameter("countrySearch");
+        listUser = userService.searchUser(countrySearch);
+        request.setAttribute("listUser", listUser);
+        request.getRequestDispatcher("user/list.jsp").forward(request,response);
+    }
+
+    private void deleteUser(HttpServletRequest request, HttpServletResponse response)
+    throws SQLException, IOException, ServletException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        userService.deleteUser(id);
+        List<User> listUser = userService.selectAllUsers();
+        request.setAttribute("listUser", listUser);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user/list.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user/create.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void updateUser(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void insertUser(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+
+}
---
+++ _12_jdbc_crud/quan_ly_user/src/main/java/service/UserService.java
@@ -0,0 +1,42 @@
+package service;
+
+import model.User;
+import repository.IUserRepository;
+import repository.UserRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserService implements IUserService {
+    private IUserRepository iUserRepository = new UserRepository();
+
+    @Override
+    public void insertUser(User user) throws SQLException {
+        iUserRepository.insertUser(user);
+    }
+
+    @Override
+    public User selectUser(int id) {
+        return iUserRepository.selectUser(id);
+    }
+
+    @Override
+    public List<User> selectAllUsers() {
+        return iUserRepository.selectAllUsers();
+    }
+
+    @Override
+    public boolean deleteUser(int id) throws SQLException {
+        return iUserRepository.deleteUser(id);
+    }
+
+    @Override
+    public boolean updateUser(User user) throws SQLException {
+        return iUserRepository.updateUser(user);
+    }
+
+    @Override
+    public List<User> searchUser(String country) {
+        return iUserRepository.searchUser(country);
+    }
+}
